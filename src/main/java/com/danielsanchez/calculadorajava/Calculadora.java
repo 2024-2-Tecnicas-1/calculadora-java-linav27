4bfe54b68d0e7833b59e22b8beaa827aa1d279b7
Juan <calderajuan15@gmail.com>
2024-08-22 08:23:48 -0500

    se adiciona potencia y modulo

--- src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
+++ src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
@@ -26,10 +26,16 @@ public class Calculadora {
                 // Operación división:
                 respuesta = (double) numero1 / numero2;
                 break;
-            
-            // TODO: DEBES CREAR AQUÍ LOS CASES PARA LA OPERACIÓN DE LA POTENCIA Y EL MÓDULO.
+                 case '^' :
+                // Operación división:
+                respuesta = Math.pow(numero1, numero2);
+                break;
+             case '%' :
+                // Operación modulo:
+                respuesta = (double) numero1 % numero2;
+                break;
                 
-            default:
+             default:
                 throw new IllegalArgumentException("Operación inválida.");
         }
         return respuesta;
